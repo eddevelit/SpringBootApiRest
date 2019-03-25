9dd218d51ed2f25468ab9659d46eb581357ab022
Eddevelit <edd.develit@gmail.com>
2019-03-25 13:59:07 -0600

    Escribiendo los metodos update y delete en el Controlador Backend Api Rest

--- src/main/java/com/eddo/springbootbackendapirest/controller/ClienteRestController.java
+++ src/main/java/com/eddo/springbootbackendapirest/controller/ClienteRestController.java
@@ -17,20 +17,38 @@ public class ClienteRestController {
     private IClienteService clienteService;
 
     @GetMapping("/clientes")
-    public List<Cliente> index(){
+    public List<Cliente> index() {
         return clienteService.findAll();
     }
 
     @GetMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente show(@PathVariable Long id){
+    public Cliente show(@PathVariable Long id) {
         return clienteService.findById(id);
     }
 
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+
+        Cliente clienteActual = clienteService.findById(id);
+
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+     public void delete(@PathVariable Long id){
+        clienteService.delete(id);
+     }
 }
