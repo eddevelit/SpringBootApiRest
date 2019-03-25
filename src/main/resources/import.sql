4603c071798884746a731ecc67e1bf071304b12c
Eddevelit <edd.develit@gmail.com>
2019-03-25 11:44:14 -0600

    Añadiendo datos de prueba Se agrega el archivo import.sql en la carpeta resources con datos de prueba para la clase clientes y se prueba la aplicacion entrando al endpoint localhost:8080/api/clientes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('dylan', 'schneider', 'Dy.SCHNEIDER2743@yahoo.com', '1903-02-17');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('sylas', 'norton', 'Syl.NORTO3571@mail2web.com', '1947-05-22');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('shane', 'horn', 'Shane.HO7429@yahoo.com', '1973-05-27');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('jessie', 'preston', 'Jes.PRESTO7734@yahoo.com', '1994-05-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('sylas', 'horn', 'Syla.HOR2756@live.com', '1904-02-26');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('lathan', 'short', 'Lath.SHORT2236@mail2web.com', '1912-01-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('yehuda', 'buchanan', 'Yehuda.BUCHANAN8270@mail2web.com', '1950-07-09');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('giada', 'bright', 'Gi.BRI2588@live.com', '1975-07-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('isai', 'villarreal', 'Isai.VILLARREA5285@mail2web.com', '1940-09-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('vance', 'contreras', 'Van.CONTR3493@yahoo.com', '1954-10-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('richard', 'thomas', 'Ric.THOM5987@hushmail.com', '1923-04-22');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('carlie', 'hyde', 'Carli.HY9055@mail2web.com', '1940-06-29');
