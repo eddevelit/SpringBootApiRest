973c76f182dd8e9342fa69edb33d16fda19dd0ab
Eddevelit <edd.develit@gmail.com>
2019-04-02 11:17:44 -0600

    Upload en el API REST Controller Se agrega el metodo para subir archivos en clientes controller, Se hacen las odificaciones necesarias en la clase entity

--- src/main/java/com/eddo/springbootbackendapirest/model/entity/Cliente.java
+++ src/main/java/com/eddo/springbootbackendapirest/model/entity/Cliente.java
@@ -34,6 +34,8 @@ public class Cliente implements Serializable {
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
+    private String foto;
+
 
 //    @PrePersist
     public void prePersis(){
@@ -78,6 +80,14 @@ public class Cliente implements Serializable {
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
     }
 
     private static final long serialVersionUID = 1;
