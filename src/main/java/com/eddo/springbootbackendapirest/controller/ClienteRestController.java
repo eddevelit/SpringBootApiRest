c0e5a18a054a8ef1a8932c908e72165b12d5819e
Eddevelit <edd.develit@gmail.com>
2019-04-09 17:27:51 -0500

    Quitando imports inecesarios

--- src/main/java/com/eddo/springbootbackendapirest/controller/ClienteRestController.java
+++ src/main/java/com/eddo/springbootbackendapirest/controller/ClienteRestController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,12 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
